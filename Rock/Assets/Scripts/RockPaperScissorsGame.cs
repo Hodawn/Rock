3d57ac66a4992fad24614fa25a77e70af6a8d593
Hodawn <happygy925@naver.com>
2023-11-30 21:10:44 +0900

    상대 캐릭터, 상대가 낸 이미지 추가

--- Rock/Assets/Scripts/RockPaperScissorsGame.cs
+++ Rock/Assets/Scripts/RockPaperScissorsGame.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class RockPaperScissorsGame : MonoBehaviour
 {
     public Text resultText;
+    public Image computerHandImage; // 추가된 부분
 
     private enum HandShape { Rock, Paper, Scissors };
     private HandShape playerChoice;
@@ -29,6 +30,21 @@ public class RockPaperScissorsGame : MonoBehaviour
         playerChoice = choice;
         computerChoice = (HandShape)Random.Range(0, 3); // 무작위로 컴퓨터의 선택을 생성
 
+
+        switch (computerChoice)
+        {
+            case HandShape.Rock:
+                computerHandImage.sprite = Resources.Load<Sprite>("R"); // 리소스 폴더에 있는 이미지 이름
+                break;
+            case HandShape.Paper:
+                computerHandImage.sprite = Resources.Load<Sprite>("P");
+                break;
+            case HandShape.Scissors:
+                computerHandImage.sprite = Resources.Load<Sprite>("S");
+                break;
+        }
+
+
         DetermineWinner();
     }
 
@@ -36,17 +52,17 @@ public class RockPaperScissorsGame : MonoBehaviour
     {
         if (playerChoice == computerChoice)
         {
-            resultText.text = "무승부!";
+            resultText.text = "Draw";
         }
         else if ((playerChoice == HandShape.Rock && computerChoice == HandShape.Scissors) ||
                  (playerChoice == HandShape.Paper && computerChoice == HandShape.Rock) ||
                  (playerChoice == HandShape.Scissors && computerChoice == HandShape.Paper))
         {
-            resultText.text = "승리!";
+            resultText.text = "Win";
         }
         else
         {
-            resultText.text = "패배!";
+            resultText.text = "Lose";
         }
     }
 }
